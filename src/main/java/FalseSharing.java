98de0714304d10d1e7ae3194a940a0208f93f754
martin.cong <martin.cong@zhulinkeji.com>
2024-04-21 15:58:43 +0800

    test

---
+++ src/main/java/FalseSharing.java
@@ -0,0 +1,11 @@
+/**
+ * Botaoyx.com Inc.
+ * Copyright (c) 2021-2023 All Rights Reserved.
+ */
+package PACKAGE_NAME;/**
+ *
+ * @author Martin.C
+ * @version 2023/06/13 20:38
+ */
+    public class FalseSharing {
+}
